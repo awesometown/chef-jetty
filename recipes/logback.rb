32afa2506426d8ff76a114926e5a209e85e61fe3
John Price <joprice@ea.com>
2014-03-25 11:52:33 -0400

    Updating logging settings

--- recipes/logback.rb
+++ recipes/logback.rb
@@ -1,24 +1,33 @@
-directory "#{node[:jetty][:homedir]}/lib/logging" do
-	owner node[:jetty][:user]
-	group node[:jetty][:group]
-end
-
-remote_file "#{node[:jetty][:homedir]}/lib/logging/slf4j-api-1.7.6.jar" do
+remote_file "#{node[:jetty][:homedir]}/lib/ext/slf4j-api-1.7.6.jar" do
 	source "http://search.maven.org/remotecontent?filepath=org/slf4j/slf4j-api/1.7.6/slf4j-api-1.7.6.jar"
 	owner node[:jetty][:user]
 	group node[:jetty][:group]
 	notifies :restart, "service[jetty]", :delayed
 end
 
-remote_file "#{node[:jetty][:homedir]}/lib/logging/logback-classic-1.1.1.jar" do
+remote_file "#{node[:jetty][:homedir]}/lib/ext/logback-classic-1.1.1.jar" do
 	source "http://search.maven.org/remotecontent?filepath=ch/qos/logback/logback-classic/1.1.1/logback-classic-1.1.1.jar"
 	owner node[:jetty][:user]
 	group node[:jetty][:group]
 	notifies :restart, "service[jetty]", :delayed
 end
 
-remote_file "#{node[:jetty][:homedir]}/lib/logging/logback-core-1.1.1.jar" do
+remote_file "#{node[:jetty][:homedir]}/lib/ext/logback-core-1.1.1.jar" do
 	source "http://search.maven.org/remotecontent?filepath=ch/qos/logback/logback-core/1.1.1/logback-core-1.1.1.jar"
+	owner node[:jetty][:user]
+	group node[:jetty][:group]
+	notifies :restart, "service[jetty]", :delayed
+end
+
+remote_file "#{node[:jetty][:homedir]}/lib/ext/logback-access-1.1.1.jar" do
+	source "http://search.maven.org/remotecontent?filepath=ch/qos/logback/logback-access/1.1.1/logback-access-1.1.1.jar"
+	owner node[:jetty][:user]
+	group node[:jetty][:group]
+	notifies :restart, "service[jetty]", :delayed
+end
+
+remote_file "#{node[:jetty][:homedir]}/lib/ext/logstash-logback-encoder-2.5.jar" do
+	source "http://search.maven.org/remotecontent?filepath=net/logstash/logback/logstash-logback-encoder/2.5/logstash-logback-encoder-2.5.jar"
 	owner node[:jetty][:user]
 	group node[:jetty][:group]
 	notifies :restart, "service[jetty]", :delayed
@@ -26,6 +35,13 @@ end
 
 template "#{node[:jetty][:homedir]}/etc/jetty-logging.xml" do
 	source "jetty-logging.xml.erb"
+	owner node[:jetty][:user]
+	group node[:jetty][:group]
+	notifies :restart, "service[jetty]", :delayed
+end
+
+template "#{node[:jetty][:homedir]}/resources/logback-access.xml" do
+	source "logback-access.xml.erb"
 	owner node[:jetty][:user]
 	group node[:jetty][:group]
 	notifies :restart, "service[jetty]", :delayed
